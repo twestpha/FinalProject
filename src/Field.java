aaad2e69955d58170cce236c42cda78c0294bbe5
unknown <twestpha@MZ022-37.adit.mines.edu>
2013-11-12 12:23:25 -0700

    First commit. Entirely Stubs.

---
+++ src/Field.java
@@ -0,0 +1,12 @@
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+
+public class Field extends JPanel {
+
+	public void paintComponent(Graphics g){
+		
+	}
+
+}
---
+++ src/Player.java
@@ -0,0 +1,12 @@
+import java.awt.Graphics;
+
+
+public class Player {
+	
+	int x, y;
+	char symbol;
+	
+	public void draw(Graphics g){
+		// Draws symbol, implement here!
+	}
+}
