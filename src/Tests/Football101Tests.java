fc1a3d65010c75719544b64f3205b7d644cd70e1
Marcus Bermel <mbermel7@gmail.com>
2013-11-12 14:50:12 -0700

    Restructured packages. Wrote test for player movement to a point

--- src/Tests/Football101Tests.java
+++ src/Tests/Football101Tests.java
@@ -1,19 +1,31 @@
 package Tests;
 
 import static org.junit.Assert.*;
+import junit.framework.Assert;
 
 import org.junit.Before;
 import org.junit.Test;
 
-public class Football101Tests {
+import Football101.Player;
 
+public class Football101Tests {
+	private Player p1;
 	@Before
 	public void setUp() throws Exception {
+		p1 = new Player(0,0,'O');
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testMove() {
+		p1.moveTo(50, 25);
+		Assert.assertEquals(50, p1.getX());
+		Assert.assertEquals(25, p1.getY());
+		
+		p1.moveTo(100, 50);
+		Assert.assertEquals(100, p1.getX());
+		Assert.assertEquals(50, p1.getY());
 	}
+	
+	
 
 }
