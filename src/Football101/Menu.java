94d2a2686b7447a5e847c0aef7ab13796d40d574
unknown <twestpha@MZ022-37.adit.mines.edu>
2013-11-19 12:14:22 -0700

    Started working on menu.java details

--- src/Football101/Menu.java
+++ src/Football101/Menu.java
@@ -1,28 +1,36 @@
 package Football101;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.swing.JPanel;
 
 
 public class Menu extends JPanel {
 	
 	private String selection;
+	private Map<String,String> fileData;
+	
+	public Menu( HashMap<String,String> fileData){
+		this.fileData = fileData;
+	}
 	
 	public int getNumElements(){
-        return 0;
+        return fileData.size();
     }
 	
 	public void executeSelectedPlay(){
-		
+		// Iunno yet
 	}
 
 	public String getSelection() {
-		return "";
+		return selection;
 	}
 
 	public void setSelection(String selection) {
 		// For testing, later will be for
 		// comboboxes to use
-		//this.selection = selection;
-		selection = null;
+		this.selection = selection;
 	}
 	
 	
