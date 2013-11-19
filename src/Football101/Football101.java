2dc11fb972a21be947c3962536f6d99d9e8f8a42
unknown <twestpha@MZ022-37.adit.mines.edu>
2013-11-19 12:02:47 -0700

    Reconfigured loadMenu()

--- src/Football101/Football101.java
+++ src/Football101/Football101.java
@@ -5,39 +5,40 @@ import javax.swing.JFrame;
 
 
 public class Football101 extends JFrame {
-	
+
 	private Field field;
 	private Play currentPlay;
-	
+	private Menu menu;
+
 	private ArrayList<Player> offensivePlayers;
 	private ArrayList<Player> defensivePlayers;
-	
+
 	private static final int PLAYERS_PER_TEAM = 11;
-	
+
 	public Football101() {
 		// TODO Auto-generated constructor stub
 		field = new Field();
-		
+
 		offensivePlayers = new ArrayList<Player>();
 		defensivePlayers = new ArrayList<Player>();
-		
+
 		for (int i = 0; i < PLAYERS_PER_TEAM; ++i){
 			// Fills the team array lists. The first two arguments just control where the player is at on the field
 			//	What is shown is a simple example of where they can be.
 			offensivePlayers.add(new Player(field.getWidth()*(2/5),i*field.getHeight()/PLAYERS_PER_TEAM,'O', this));
 			defensivePlayers.add(new Player(field.getWidth()*(4/5),i*field.getHeight()/PLAYERS_PER_TEAM,'X', this));
 		}
-		
+
 	}
 
 	public void clear(){
 
 	}
-	
+
 	public void executePlay(Play target){
-		
+
 	}
-	
+
 	public void loadPlay(String filename){
 	}
 
@@ -48,14 +49,22 @@ public class Football101 extends JFrame {
 	public ArrayList<Player> getDefensivePlayers() {
 		return defensivePlayers;
 	}
-	
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
-	
+
 	public Field getField(){
 		return field;
+	}
+	
+    public void loadMenu(String Filename){
+        menu = new Menu();
+    }
+
+	public Menu getMenu() {
+		return menu;
 	}
 
 
