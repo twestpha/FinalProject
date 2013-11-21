a55f6af7aa23e7b73d2a8530df3085bd87a6d9b7
unknown <twestpha@MZ022-37.adit.mines.edu>
2013-11-21 12:15:07 -0700

    Drawing the lines on the field

--- src/Football101/Field.java
+++ src/Football101/Field.java
@@ -15,9 +15,11 @@ public class Field extends JPanel {
 	public void paintComponent(Graphics g){
 		super.paintComponents(g);
 		g.setColor(new Color(57, 128, 71));
-		
-		// Dynamic Scaling
 		g.fillRect(0,0,getSize().width,getSize().height);
+		g.setColor(Color.WHITE);
+		g.fillRect((getSize().width/2)-3,30,6,getSize().height - 60);
+		g.fillRect((getSize().width/4)-3,30,6,getSize().height - 60);
+		g.fillRect((getSize().width/2)+(getSize().width/4)-3,30,6,getSize().height - 60);
 	}
 
 }
