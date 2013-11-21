e17e4439971c6e048989ef1ce739239fdfa57585
unknown <twestpha@MZ022-37.adit.mines.edu>
2013-11-21 12:08:51 -0700

    Added jframe utilities, locked window size

--- src/Football101/Field.java
+++ src/Football101/Field.java
@@ -1,4 +1,5 @@
 package Football101;
+import java.awt.Color;
 import java.awt.Graphics;
 
 import javax.swing.JPanel;
@@ -12,7 +13,11 @@ public class Field extends JPanel {
 	}
 
 	public void paintComponent(Graphics g){
+		super.paintComponents(g);
+		g.setColor(new Color(57, 128, 71));
 		
+		// Dynamic Scaling
+		g.fillRect(0,0,getSize().width,getSize().height);
 	}
 
 }
