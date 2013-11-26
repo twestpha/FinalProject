3558a20d7ccb24930ebaf93bf8e75323fe57ee3a
unknown <twestpha@MZ022-37.adit.mines.edu>
2013-11-26 11:04:58 -0700

    Added testing grid

--- src/Football101/Field.java
+++ src/Football101/Field.java
@@ -13,6 +13,7 @@ public class Field extends JPanel {
 	}
 
 	public void paintComponent(Graphics g){
+		final int SPACING = 32;
 		super.paintComponents(g);
 		g.setColor(new Color(57, 128, 71));
 		g.fillRect(0,0,getSize().width,getSize().height);
@@ -20,6 +21,15 @@ public class Field extends JPanel {
 		g.fillRect((getSize().width/2)-3,30,6,getSize().height - 60);
 		g.fillRect((getSize().width/4)-3,30,6,getSize().height - 60);
 		g.fillRect((getSize().width/2)+(getSize().width/4)-3,30,6,getSize().height - 60);
+		// TEST GRID.
+		for(int i = 0; i < getSize().width/SPACING+1; ++i){
+			g.drawLine(SPACING*i, 0, SPACING*i, getSize().height);
+		}
+		for(int j = 0; j < getSize().height/SPACING+1; ++j){
+			g.drawLine(0,SPACING*j,getSize().width,SPACING*j);
+		}
+		
+		
 	}
 
 }
