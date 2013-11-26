8461b6abb0e5f5da026088250ad7c9fefdf28e78
unknown <twestpha@MZ022-37.adit.mines.edu>
2013-11-26 11:19:08 -0700

    Added spacing, numbers to testing grid

--- src/Football101/Field.java
+++ src/Football101/Field.java
@@ -13,14 +13,14 @@ public class Field extends JPanel {
 	}
 
 	public void paintComponent(Graphics g){
-		final int SPACING = 32;
+		final int SPACING = 64;
 		super.paintComponents(g);
 		g.setColor(new Color(57, 128, 71));
 		g.fillRect(0,0,getSize().width,getSize().height);
 		g.setColor(Color.WHITE);
-		g.fillRect((getSize().width/2)-3,30,6,getSize().height - 60);
+		/*g.fillRect((getSize().width/2)-3,30,6,getSize().height - 60);
 		g.fillRect((getSize().width/4)-3,30,6,getSize().height - 60);
-		g.fillRect((getSize().width/2)+(getSize().width/4)-3,30,6,getSize().height - 60);
+		g.fillRect((getSize().width/2)+(getSize().width/4)-3,30,6,getSize().height - 60);*/
 		// TODO: TEST GRID. REMOVE BEFORE RELEASE
 		for(int i = 0; i < getSize().width/SPACING+1; ++i){
 			g.drawLine(SPACING*i, 0, SPACING*i, getSize().height);
@@ -28,8 +28,11 @@ public class Field extends JPanel {
 		for(int j = 0; j < getSize().height/SPACING+1; ++j){
 			g.drawLine(0,SPACING*j,getSize().width,SPACING*j);
 		}
-		
-		
+		for(int k = 0; k < getSize().width/SPACING+1; ++k){
+			for(int l = 0; l < getSize().height/SPACING+1; ++l){
+				g.drawString((k*SPACING)+","+(l*SPACING), (SPACING*k)+1, (SPACING*l)+11);
+			}
+		}
+		// End of testing grid
 	}
-
 }
